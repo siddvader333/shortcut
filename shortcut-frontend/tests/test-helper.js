89d90732cf5e94004f70c56a7263e1444ae3308c
Siddharth Vijay <siddharth.vijay333@gmail.com>
2020-12-27 22:42:47 -0500

    Added ruby on rails server and created new ember project to change folder name to frontend (consistency)

--- shortcut-frontend/tests/test-helper.js
+++
@@ -1,8 +0,0 @@
-import Application from 'shortcut-frontend/app';
-import config from 'shortcut-frontend/config/environment';
-import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
-
-setApplication(Application.create(config.APP));
-
-start();
